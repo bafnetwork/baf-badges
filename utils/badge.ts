8b901d4895b3d888d1a155c476c98ec9f215a44e
Sebastien La Duca <sladuca777@gmail.com>
2021-08-05 12:23:02 +0200

    implement badge document upload / download

--- utils/badge.ts
+++ utils/badge.ts
@@ -1,8 +1,11 @@
 import 'reflect-metadata';
 import { AnyT, jsonObject, jsonMember, TypedJSON, jsonMapMember, jsonArrayMember } from 'typedjson';
-import { ContractMethodNotInitializedError, DeserializationError } from './errors';
+import { BadgeDocumentNotFoundError, ContractMethodNotInitializedError, DeserializationError, MalformedResponseError, NFTMediaIntegrityError, NFTReferenceIntegrityError, ReceivedInternalServerError, UnexpectedStatusError } from './errors';
 import uuid from 'uuid';
 import { sha256 } from './crypto';
+
+const UPLOAD_BADGE_PATH = 'api/uploadDocument';
+const BADGE_DOCUMENT_BASE_PATH = 'storageapi.fleek.co/sladuca-team-bucket'
 
 @jsonObject
 export class NearNFT {
@@ -13,7 +16,7 @@ export class NearNFT {
 	public owner_id!: string;
 
 	@jsonMember
-	public metadata!: null | NearNFTMetadata;
+	public metadata!: NearNFTMetadata;
 
 	@jsonMember(AnyT)
 	public approved_account_ids: any;
@@ -22,16 +25,16 @@ export class NearNFT {
 @jsonObject
 export class NearNFTMetadata {
 	@jsonMember
-	public title!: string | null;
+	public title!: string;
 
 	@jsonMember
-	public description!: string | null;
+	public description!: string;
 
 	@jsonMember
-	public media!: string | null;
+	public media!: string;
 
 	@jsonMember
-	public media_hash!: string | null;
+	public media_hash!: string;
 
 	@jsonMember
 	public copies!: number | null;
@@ -52,10 +55,10 @@ export class NearNFTMetadata {
 	public extra!: string | null;
 
 	@jsonMember
-	public reference!: string | null;
+	public reference!: string;
 
 	@jsonMember
-	public referenceHash!: string | null;
+	public reference_hash!: string;
 }
 
 export type RequirementType = 'SINGLE' | 'ONE_OF' | 'N_OF' | 'ALL_OF';
@@ -115,40 +118,79 @@ const badgeDocumentSerializer = new TypedJSON(BafBadgeDocument);
 
 export async function getBadgeNFT(badgeID: string): Promise<NearNFT> {
 	if (!window.contract.nft_token) {
-		throw ContractMethodNotInitializedError('nft_token', 'getBadge');
+		throw ContractMethodNotInitializedError('nft_token');
 	}
 
 	const nftRaw = await window.contract.nft_token({ token_id: badgeID });
 
 	const nft = nftSerializer.parse(nftRaw);
 	if (!nft) {
-		throw DeserializationError('NearNFT', nftRaw, 'getBadge');
+		throw DeserializationError('NearNFT', nftRaw);
 	}
 
 	return nft;
 }
 
-export async function getBadgeDocument(badgeID: string): Promise<BadgeDocument> {
-	throw 'unimplemented'
+// return fleek pubic url
+export async function uploadBadgeDocument(document: BafBadgeDocument): Promise<string> {
+	const request = new Request(UPLOAD_BADGE_PATH, {
+		method: 'POST',
+		body: badgeDocumentSerializer.stringify(document)
+	});
+
+	const response = await window.fetch(request);
+	switch (response.status) {
+		case 201:
+			const body = await response.json();
+			if (!body.url) {
+				throw MalformedResponseError(UPLOAD_BADGE_PATH, 'response body missing \'url\' property');
+			}
+			return body.url;
+		case 500:
+			throw ReceivedInternalServerError(UPLOAD_BADGE_PATH);
+		default:
+			throw UnexpectedStatusError(UPLOAD_BADGE_PATH, response.status);
+	}
+}
+
+export async function getBadgeDocument(nft: NearNFT): Promise<BafBadgeDocument> {
+	const url = `${BADGE_DOCUMENT_BASE_PATH}/${nft.metadata.reference}`;
+	const response = await window.fetch(url);
+
+	switch (response.status) {
+		case 200:
+			const body = await response.json();
+			const document = badgeDocumentSerializer.parse(body);
+			if (!document) {
+				throw DeserializationError('BafBadgeDocument', body);
+			}
+			return document;
+		case 404: 
+			throw BadgeDocumentNotFoundError(nft.token_id, url);
+		default:
+			throw UnexpectedStatusError(BADGE_DOCUMENT_BASE_PATH, response.status);
+	}
 }
 
 export async function getBadge(badgeID: string): Promise<BafBadge> {
-	const [onChain, offChain] = await Promise.all([
-		getBadgeNFT(badgeID),
-		getBadgeDocument(badgeID)
-	]);
+	const onChain = await getBadgeNFT(badgeID);
+
+	await verifyNFTReferenceIntegrity(onChain);
+	await verifyNFTMediaIntegrity(onChain);
+
+	const offChain = await getBadgeDocument(onChain);
 
 	return { onChain, offChain }	
 }
 
 export async function mintBadge(recipientAccountID: string, badgeMediaURL: string, args: BafBadgeCreateArgs): Promise<BafBadge> {
 	if (!window.contract.mint) {
-		throw ContractMethodNotInitializedError('mint', 'mintBadge');
+		throw ContractMethodNotInitializedError('mint');
 	}
 
 	const token_id = uuid.v4().toString();
 
-	const ceramicStreamID = await uploadBadgeDocument(args.offChain);
+	const documentPublicUrl = await uploadBadgeDocument(args.offChain);
 
 	const token_metadata = {
 		title: args.title,
@@ -156,8 +198,8 @@ export async function mintBadge(recipientAccountID: string, badgeMediaURL: strin
 		media: badgeMediaURL,
 		media_hash: await sha256(badgeMediaURL),
 		issued_at: (new Date()).toString(),
-		reference: ceramicStreamID,
-		referenceHash: await sha256(ceramicStreamID),
+		reference: documentPublicUrl,
+		reference_hash: await sha256(documentPublicUrl),
 	}
 
 	const nftRaw = await window.contract.mint(
@@ -172,7 +214,7 @@ export async function mintBadge(recipientAccountID: string, badgeMediaURL: strin
 
 	const nft = nftSerializer.parse(nftRaw);
 	if (!nft) {
-		throw DeserializationError('NearNFT', nftRaw, 'mintBadge');
+		throw DeserializationError('NearNFT', nftRaw);
 	}
 
 	return {
@@ -181,8 +223,16 @@ export async function mintBadge(recipientAccountID: string, badgeMediaURL: strin
 	}
 }
 
-// return ceramic streamID
-export async function uploadBadgeDocument(document: BafBadgeDocument): Promise<string> {
-	throw 'unimplemented'
+export async function verifyNFTReferenceIntegrity(nft: NearNFT): Promise<void> {
+	const hash = await sha256(nft.metadata.reference);
+	if (hash !== nft.metadata.reference_hash) {
+		throw NFTReferenceIntegrityError(nft.token_id);
+	}
 }
 
+export async function verifyNFTMediaIntegrity(nft: NearNFT): Promise<void> {
+	const hash = await sha256(nft.metadata.media);
+	if (hash !== nft.metadata.media_hash) {
+		throw NFTMediaIntegrityError(nft.token_id);
+	}
+}
--- utils/errors.ts
+++ utils/errors.ts
@@ -1,17 +1,46 @@
-function createError(name: string, routine: string, message: string, ): Error {
+function createError(name: string, message: string): Error {
 	// possibly do other things here
-	return new Error(`${name} in ${routine}: ${message}`);
+	return new Error(`${name}: ${message}`);
 }
 
-export const ContractMethodNotInitializedError = (method: string, routine: string) => createError(
+export const ContractMethodNotInitializedError = (method: string) => createError(
 	"ContractMethodNotInitializedError",
-	routine,
 	`contract method '${method}' is not initialized`
 );
 
-export const DeserializationError = (_type: string, raw: any, routine: string) => creaateError(
+export const DeserializationError = (_type: string, raw: any) => createError(
 	'DeserializationError',
-	routine,
 	`failed to deserialize ${raw} to ${_type}`
 )
+
+export const MalformedResponseError = (url: string, reason: string) => createError(
+	'MalformedResponseError',
+	`malformed response from request to ${url}: ${reason}`
+);
+
+export const ReceivedInternalServerError = (url: string) => createError(
+	'ReceivedInternalServerError',
+	`received internal server error from request to ${url}`
+);
+
+export const UnexpectedStatusError = (url: string, status: number) => createError(
+	'UnexpectedStatusError',
+	`received unexpected status code ${status} from request to ${url}`
+);
+
+export const NFTReferenceIntegrityError = (badgeID: string) => createError(
+	'NFTReferenceIntegrityError',
+	`badge ${badgeID}'s metadata.reference_hash does not match metadata.reference!`
+);
+
+export const NFTMediaIntegrityError = (badgeID: string) => createError(
+	'NFTMediaIntegrityError',
+	`badge ${badgeID}'s metadata.media_hash does not match metadata.media!`
+);
+
+export const BadgeDocumentNotFoundError = (badgeID: string, url: string) => createError(
+	'BadgeDocumentNotFoundError',
+	`metadata for badge ${badgeID} not found at ${url}`
+);
+
 
