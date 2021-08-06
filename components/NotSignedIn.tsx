3b235d41d9df267ae968153bca26302055ca93d7
Sebastien La Duca <sladuca777@gmail.com>
2021-08-06 16:09:04 +0200

    fix quot

--- components/NotSignedIn.tsx
+++ components/NotSignedIn.tsx
@@ -3,19 +3,9 @@ import { login } from '../utils/init';
 export function NotSignedIn() {
 	return (
 		<main>
-        <h1>Welcome to NEAR!</h1>
+        <h1>It looks like you&apos;re not signed in.</h1>
         <p>
-          To make use of the NEAR blockchain, you need to sign in. The button
-          below will sign you in using NEAR Wallet.
-        </p>
-        <p>
-          By default, when your app runs in "development" mode, it connects
-          to a test network ("testnet") wallet. This works just like the main
-          network ("mainnet") wallet, but the NEAR Tokens on testnet aren't
-          convertible to other currencies – they're just for testing!
-        </p>
-        <p>
-          Go ahead and click the button below to try it out:
+          Please sign in with NEAR wallet to proceed.
         </p>
         <p style={{ textAlign: 'center', marginTop: '2.5em' }}>
           <button onClick={login}>Sign in</button>
