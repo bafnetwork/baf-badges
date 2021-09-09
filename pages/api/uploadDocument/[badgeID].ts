e30bc1225b786f9cf0359510b9a902e843935713
Sebastien La Duca <sladuca777@gmail.com>
2021-09-08 21:36:16 -0400

    remove external body-parser

--- pages/api/uploadDocument/[badgeID].ts
+++ pages/api/uploadDocument/[badgeID].ts
@@ -2,7 +2,6 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { upload } from '@fleekhq/fleek-storage-js';
 import { getFleekAPIKey, getFleekAPISecret } from '../../../utils/fleek';
-import bodyParser from 'body-parser';
 
 interface UploadOkResponse {
   url: string;
@@ -14,27 +13,12 @@ interface ErrorResponse {
 
 type UploadResponse = UploadOkResponse | ErrorResponse;
 
-function runMiddleware(req: any, res: any, fn: any) {
-  return new Promise((resolve, reject) => {
-    fn(req, res, (result: any) => {
-      if (result instanceof Error) {
-        return reject(result)
-      }
-
-      return resolve(result)
-    })
-  })
-}
-
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<UploadResponse>
 ) {
 
-  await runMiddleware(req, res, bodyParser.json());
-
   const { badgeID } = req.query;
-
 
   try {
     const uploadedFile = await upload({
